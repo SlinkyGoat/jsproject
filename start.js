171e70f4c323d6b844e0639599fee126810b9816
SlinkyGoat <adamustby@gmail.com>
2024-08-06 16:17:22 -0500

    do more later, I don't know what to do right now

---
+++ start.js
@@ -0,0 +1,18 @@
+// js projects to make and add to here based on user input
+// calculator
+// number guessing game
+// hangman
+// palindrome checker
+// simmple encrypt/decrypt (such as ceaser cypher or more complicated if wanted)
+// weather CLI to check weather in console using a weather API
+// import calc from './calculator';
+
+
+console.log("Welcome to my js project that includes many mini projects!\n");
+
+let input = "";
+while(input != "quit"){
+    input = require("Please enter what you would like to do: ");
+    console.log(input);
+}
+
